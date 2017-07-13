aa066d7243d97b45f9714b10a45b344ab137a4d5
Alex Rice <alexander.rice@sjc.ox.ac.uk>
2017-07-13 14:52:16 +0100

    First Draft

--- MyBot.cs
+++ MyBot.cs
@@ -1,71 +1,105 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Xml.Schema;
 using Battleships.Player.Interface;
 
 namespace BattleshipBot
 {
-  public class MyBot : IBattleshipsBot
-  {
-    private IGridSquare lastTarget;
-
-    public IEnumerable<IShipPosition> GetShipPositions()
+    public class MyBot : IBattleshipsBot
     {
-      lastTarget = null; // Forget all our history when we start a new game
+        private IGridSquare lastTarget;
+        private Random randomGen = new Random();
+        private static char[] letters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
-      return new List<IShipPosition>
-      {
-        GetShipPosition('A', 1, 'A', 5), // Aircraft Carrier
-        GetShipPosition('C', 1, 'C', 4), // Battleship
-        GetShipPosition('E', 1, 'E', 3), // Destroyer
-        GetShipPosition('G', 1, 'G', 3), // Submarine
-        GetShipPosition('I', 1, 'I', 2)  // Patrol boat
-      };
+        public IEnumerable<IShipPosition> GetShipPositions()
+        {
+            lastTarget = null; // Forget all our history when we start a new game
+            List<ShipPositionExtended> ships = null;
+            while (!Valid(ships))
+            {
+                ships = new List<ShipPositionExtended>
+                {
+                    GetShipPosition(5), // Aircraft Carrier
+                    GetShipPosition(4), // Battleship
+                    GetShipPosition(3), // Destroyer
+                    GetShipPosition(2), // Submarine
+                    GetShipPosition(2) // Patrol boat
+                };
+            }
+            return ships;
+        }
+
+        private ShipPositionExtended GetShipPosition(int length)
+        {
+            return new ShipPositionExtended(new GridSquare(letters[randomGen.Next(9)],randomGen.Next(1,10)),randomGen.Next(3),length);
+        }
+
+        private static bool Valid(List<ShipPositionExtended> ships)
+        {
+            if (ships == null)
+            {
+                return false;
+            }
+            foreach (var ship in ships)
+            {
+                if (!ship.isValid())
+                    return false;
+            }
+            for (int column = 1; column <= 10; column++)
+            {
+                foreach (var row in letters)
+                {
+                    var grid = new GridSquare(row, column);
+                    if (ships.Select(x => x.Contains(grid) ? 1 : 0).Aggregate((x, y) => x + y) > 1)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public IGridSquare SelectTarget()
+        {
+            var nextTarget = GetNextTarget();
+            lastTarget = nextTarget;
+            return nextTarget;
+        }
+
+        private IGridSquare GetNextTarget()
+        {
+            if (lastTarget == null)
+            {
+                return new GridSquare('A', 1);
+            }
+
+            var row = lastTarget.Row;
+            var col = lastTarget.Column + 1;
+            if (lastTarget.Column != 10)
+            {
+                return new GridSquare(row, col);
+            }
+
+            row = (char) (row + 1);
+            if (row > 'J')
+            {
+                row = 'A';
+            }
+            col = 1;
+            return new GridSquare(row, col);
+        }
+
+        public void HandleShotResult(IGridSquare square, bool wasHit)
+        {
+            // Ignore whether we're successful
+        }
+
+        public void HandleOpponentsShot(IGridSquare square)
+        {
+            // Ignore what our opponent does
+        }
+
+        public string Name => "Barely Floating";
     }
-
-    private static ShipPosition GetShipPosition(char startRow, int startColumn, char endRow, int endColumn)
-    {
-      return new ShipPosition(new GridSquare(startRow, startColumn), new GridSquare(endRow, endColumn));
-    }
-
-    public IGridSquare SelectTarget()
-    {
-      var nextTarget = GetNextTarget();
-      lastTarget = nextTarget;
-      return nextTarget;
-    }
-
-    private IGridSquare GetNextTarget()
-    {
-      if (lastTarget == null)
-      {
-        return new GridSquare('A', 1);
-      }
-
-      var row = lastTarget.Row;
-      var col = lastTarget.Column + 1;
-      if (lastTarget.Column != 10)
-      {
-        return new GridSquare(row, col);
-      }
-
-      row = (char)(row + 1);
-      if (row > 'J')
-      {
-        row = 'A';
-      }
-      col = 1;
-      return new GridSquare(row, col);
-    }
-
-    public void HandleShotResult(IGridSquare square, bool wasHit)
-    {
-      // Ignore whether we're successful
-    }
-
-    public void HandleOpponentsShot(IGridSquare square)
-    {
-      // Ignore what our opponent does
-    }
-
-    public string Name => "Simple Example Bot";
-  }
 }
---
+++ ShipPositionExtended.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Battleships.Player.Interface;
+
+namespace BattleshipBot
+{
+    class ShipPositionExtended : IShipPosition
+    {
+        private bool horizontal;
+        public IGridSquare StartingSquare { get;  }
+        public IGridSquare EndingSquare { get; }
+
+        private ShipPositionExtended() {}
+
+        public ShipPositionExtended(IGridSquare start, int direction, int length)
+        {
+            StartingSquare = start;
+            switch (direction)
+            {
+                case 0:
+                    horizontal = true;
+                    EndingSquare = new GridSquare((char)(StartingSquare.Row + length - 1), StartingSquare.Column);
+                    break;
+                case 1:
+                    horizontal = false;
+                    EndingSquare = new GridSquare(StartingSquare.Row, StartingSquare.Column + length - 1);
+                    break;
+                case 2:
+                    horizontal = true;
+                    EndingSquare = new GridSquare((char)(StartingSquare.Row - length + 1), StartingSquare.Column);
+                    break;
+                case 3:
+                    horizontal = false;
+                    EndingSquare = new GridSquare(StartingSquare.Row, StartingSquare.Column-length +1);
+                    break;
+            }
+        }
+
+        public bool isValid()
+        {
+            return Between('A', 'I', StartingSquare.Row) && Between('A', 'I', EndingSquare.Row) &&
+                   Between(1, 10, StartingSquare.Column) && Between(1, 10, EndingSquare.Column);
+        }
+        public bool Contains(IGridSquare square)
+        {
+            if (horizontal && square.Row == StartingSquare.Row)
+            {
+                return Between(StartingSquare.Column, EndingSquare.Column, square.Column);
+            }
+            else if (!horizontal && square.Column == StartingSquare.Column)
+            {
+                return Between(StartingSquare.Row, EndingSquare.Row, square.Row);
+            }
+            else return false;
+        }
+
+        private static bool Between<T>(T start, T end, T test) where T : IComparable
+        {
+            if (start.CompareTo(end) <= 0)
+            {
+                return start.CompareTo(test) <= 0 && test.CompareTo(end) <= 0;
+            }
+            else
+            {
+                return end.CompareTo(test) <= 0 && test.CompareTo(start) <= 0;
+            }
+        }
+    }
+}
